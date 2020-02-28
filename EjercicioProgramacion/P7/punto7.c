2088774f6a8375c2430e54968d784e726b845fc9
deiry <sofia.navas96@gmail.com>
2020-02-28 12:57:39 -0500

    upload folders

--- EjercicioProgramacion/P7/punto7.c
+++ EjercicioProgramacion/P7/punto7.c
@@ -2,4 +2,5 @@
  *  Dadas las notas de los alumnos de un colegio en el primer curso de bachillerato,
  *  en las diferentes asignaturas (5, por comodidad), se trata de calcular la media 
  * de cada alumno, la media de cada asignatura, la media total de la clase y ordenar 
- * los alumnos por orden decreciente de notas medias individuales.*/
+ * los alumnos por orden decreciente de notas medias individuales.
+ * Nota: utilizar como algoritmo de ordenación el método Shell.*/
