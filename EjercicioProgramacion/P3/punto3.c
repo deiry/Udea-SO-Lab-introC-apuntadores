85572bff44451850c9717ce85804c9b91be8b13c
deiry <sofia.navas96@gmail.com>
2020-02-29 23:04:46 -0500

    add P1, P2, P3 done

--- EjercicioProgramacion/P3/punto3.c
+++ EjercicioProgramacion/P3/punto3.c
@@ -1,4 +1,27 @@
-/**Obtener la subcadena de una cadena dada a partir de un subindice siguiendo la siguiente funcion*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+char *obtenerSubcadena(char *array, int index);
+void test(void);
+
+int main(){
+  test();
+  return 0;
+}
+
+void test(void){
+  char *p1 = "Hola,¿qué tal? ¿cómo estás?";
+  char *p2;
+  p2 = obtenerSubcadena(p1, 5);
+  printf("%s\n",p1);    // Imprime: Hola que tal
+  printf("%s\n",p2);    // Imprime: que tal
+
+}
+
+/**Obtener la subcadena de una cadena dada a partir 
+ * de un subindice siguiendo la siguiente funcion*/
 /**
  *   @brief  Obtiene una subcadena a tomada a partir de un subindice asociado a una subcadena
  *
@@ -7,5 +30,18 @@
  *   @return un apuntador a la posicion inicial de la subcadena o NULL si el tamaño de index supera a la longitud de la cadena
  */
 char *obtenerSubcadena(char *array, int index) {
-  // Coloque su codigo aqui...
+
+    int cont = 0;
+    if(index < strlen(array)){
+      while (*array != '\0'){
+      if(cont == index){
+        return array;
+      }
+      cont++;
+      array++;
+    }
+    }
+    
+    return array;
+    
 }
